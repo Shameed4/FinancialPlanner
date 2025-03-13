f6649e70e01c7cea7dbdba6aab106bd814f613a3
Sean Erfan <seanerfan@gmail.com>
2025-03-13 01:40:34 -0400

    Made starting point

---
+++ src/app/samplePage/page.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function SamplePage() {
+    return <div>Welcome to sample page! <Link href="/" className="text-blue-700">Go back to home page!</Link></div>
+}
