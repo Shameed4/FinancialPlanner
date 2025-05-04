1e7b843ab755397134c580cb03acb5e8983df744
Sean Erfan <seanerfan@gmail.com>
2025-05-03 23:00:09 -0400

    Threading complete, fixed google auth  bug

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,9 @@
 import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
     providers: [
-      GoogleProvider({
+      GoogleProvider.default({
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET,
       }), 
@@ -11,5 +11,5 @@ export const authOptions = {
     secret: process.env.SECRET
 }
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth.default(authOptions)
 export { handler as GET, handler as POST }
